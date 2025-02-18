5a8ec36b81613b827bb6e55f34096de8ce3e975b
Guilherme Puszkarek <guipuszkarek@gmail.com>
2025-02-18 17:49:43 -0300

    improve

--- apps/www/src/app/elements/notes-list/notes-list.component.ts
+++ apps/www/src/app/elements/notes-list/notes-list.component.ts
@@ -1,8 +1,15 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component, input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, input, signal } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { BaseNote } from '@api-interfaces';
 import { IconComponent } from '../../primitives/icon/icon.component';
+
+const GREEN_MANTIS_NOTE_COLOR = '#66cb62';
+const BLUE_KOI_NOTE_COLOR = '#62a7cb';
+const MEDIUM_PURPLE_NOTE_COLOR = '#8b62cb';
+const INDIAN_RED_NOTE_COLOR = '#cb6262';
+const CYAN_DOWNY_NOTE_COLOR = '#62cbc9';
+const BASKET_BALL_ORANGE_NOTE_COLOR = '#FF7F50';
 
 @Component({
   selector: 'app-notes-list',
@@ -12,10 +19,23 @@ import { IconComponent } from '../../primitives/icon/icon.component';
   imports: [CommonModule, RouterModule, IconComponent],
 })
 export class NotesListComponent {
+  private colors = [
+    GREEN_MANTIS_NOTE_COLOR,
+    BLUE_KOI_NOTE_COLOR,
+    MEDIUM_PURPLE_NOTE_COLOR,
+    INDIAN_RED_NOTE_COLOR,
+    CYAN_DOWNY_NOTE_COLOR,
+    BASKET_BALL_ORANGE_NOTE_COLOR,
+  ];
+  private _colorIndex = 0;
+
   public readonly notes = input.required<ReadonlyArray<BaseNote>>();
 
   // TODO: add color to the note
-  public randomNoteColor(): string {
-    return `var(--note-color-${Math.floor(Math.random() * 6) + 1})`;
+  public getNextColor(): string {
+    const color = this.colors[this._colorIndex] ?? '#66cb62';
+    console.log('color', color);
+    this._colorIndex = (this._colorIndex + 1) % this.colors.length;
+    return color;
   }
 }
